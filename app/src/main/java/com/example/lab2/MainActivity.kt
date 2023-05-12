cc0e5c942242f293278e39e7ea8e9ce30bc45059
AvatarSakh <vlad.polovnikov.02@mail.ru>
2023-05-12 22:15:47 +1100

    final

--- app/src/main/java/com/example/lab2/MainActivity.kt
+++ app/src/main/java/com/example/lab2/MainActivity.kt
@@ -1,22 +1,12 @@
 package com.example.lab2
 
-import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
-
-private const val TAG = "MainActivity"
-private const val KEY_CURRENTINDEX = "current_index"
-private const val KEY_COUNTANSWER = "count_answer"
-private const val KEY_CORRECTANSWER = "correct_answer"
-private const val KEY_GETANSWER = "get_answer"
 
 
 class MainActivity : AppCompatActivity() {
@@ -27,22 +17,19 @@ class MainActivity : AppCompatActivity() {
     private lateinit var questionTextView: TextView
     private lateinit var prevButton: ImageButton
 
-    private val quizViewModel: QuizViewModel by lazy {
-        ViewModelProviders.of(this).get(QuizViewModel::class.java)
-    }
+    private val questionBank = listOf(
+        Question(R.string.question_australia,true),
+        Question(R.string.question_oceans,true),
+        Question(R.string.question_mideast,false),
+        Question(R.string.question_africa,false),
+        Question(R.string.question_americas,true),
+        Question(R.string.question_asia,true))
+
+    private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d(TAG,"OnCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
-
-        if (savedInstanceState != null) {
-            quizViewModel.currentIndex = savedInstanceState.getInt(KEY_CURRENTINDEX,0)
-            quizViewModel.countAnswers = savedInstanceState.getInt(KEY_COUNTANSWER,0)
-            quizViewModel.countCorrectAnswers = savedInstanceState.getDouble(KEY_CORRECTANSWER,0.0)
-            quizViewModel.getAnswer = savedInstanceState?.getBooleanArray(KEY_GETANSWER) ?: BooleanArray(5)
-        }
-
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
@@ -57,92 +44,40 @@ class MainActivity : AppCompatActivity() {
             checkAnswer(false)
         }
         nextButton.setOnClickListener {
-            quizViewModel.currentIndex = (quizViewModel.currentIndex + 1) % quizViewModel.questionBank.size
+            currentIndex = (currentIndex+1) % questionBank.size
             questionUpdate()
         }
 
         questionTextView.setOnClickListener {
-            quizViewModel.currentIndex = (quizViewModel.currentIndex + 1) % quizViewModel.questionBank.size
+            currentIndex = (currentIndex + 1) % questionBank.size
             questionUpdate()
         }
 
         prevButton.setOnClickListener{
-            if (quizViewModel.currentIndex == 0) {
-                quizViewModel.currentIndex = quizViewModel.questionBank.size - 1
-            }
-            else{
-                quizViewModel.currentIndex--
-            }
+            if (currentIndex == 0)
+                currentIndex = questionBank.size-1
+            else
+                currentIndex--
             questionUpdate()
         }
         questionUpdate()
     }
 
-    override fun onStart(){
-        super.onStart()
-        Log.d(TAG, "OnStart() called")
-    }
-
-    override fun onResume(){
-        super.onResume()
-        Log.d(TAG, "OnResume() called")
-    }
-
-    override fun onPause(){
-        super.onPause()
-        Log.d(TAG, "OnPause called")
-    }
-    override fun onSaveInstanceState(savedInstanceState: Bundle) {
-        super.onSaveInstanceState(savedInstanceState)
-        Log.i(TAG, "onSaveInstanceState")
-        savedInstanceState.putInt(KEY_CURRENTINDEX, quizViewModel.currentIndex)
-        savedInstanceState.putDouble(KEY_CORRECTANSWER, quizViewModel.countCorrectAnswers)
-        savedInstanceState.putInt(KEY_COUNTANSWER, quizViewModel.countAnswers)
-        savedInstanceState.putBooleanArray(KEY_GETANSWER,quizViewModel.getAnswer)
-    }
-    override fun onStop(){
-        super.onStop()
-        Log.d(TAG, "OnStop() called")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "OnDestroy() called")
-    }
-
     private fun questionUpdate(){
-        val questionTextResId = quizViewModel.questionBank[quizViewModel.currentIndex].textResId
+        val questionTextResId = questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
-
-        if(quizViewModel.getAnswer[quizViewModel.currentIndex]){
-            trueButton.isEnabled = false
-            falseButton.isEnabled = false
-        }
-        else{
-            trueButton.isEnabled = true
-            falseButton.isEnabled = true
-        }
     }
 
     private fun checkAnswer(userAnswer: Boolean){
-        val correctAnswer = quizViewModel.questionBank[quizViewModel.currentIndex].answer
-        trueButton.isEnabled = false
-        falseButton.isEnabled = false
-        quizViewModel.getAnswer[quizViewModel.currentIndex] = true
-        quizViewModel.countAnswers++
+        val correctAnswer = questionBank[currentIndex].answer
 
-        if (userAnswer == correctAnswer) {
-            quizViewModel.countCorrectAnswers++
+        val messageResId = if (userAnswer == correctAnswer) {
+            R.string.correct_toast
+        } else{
+            R.string.incorrect_toast
         }
 
-        if (quizViewModel.countAnswers == quizViewModel.questionBank.size){
-
-            val value = (quizViewModel.countCorrectAnswers/(quizViewModel.questionBank.size))*100
-
-            var result = "Correct answers is: " + Math.round(value) + "%"
-            Toast.makeText(this,result,Toast.LENGTH_SHORT).show()
-        }
-
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
     }
 
 }
